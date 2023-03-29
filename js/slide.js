e4eb2fa1b76a7e2e530f629f0522f40ff169fed2
felipebrunner <felipebrunner@gmail.com>
2023-03-29 14:52:12 -0400

    atualização

---
+++ js/slide.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const slideWrapper = document.querySelector('[data-slide="wrapper"]')
+
+const slideList = document.querySelector('[data-slide="list"]')
+
+const slideItems = document.querySelectorAll('[data-slide="item"]')
+
+slideItems.forEach(function(slideItem, index) {
+    slideItem.addEventListener('mousedown', function())
+    
+
+})
+ 
